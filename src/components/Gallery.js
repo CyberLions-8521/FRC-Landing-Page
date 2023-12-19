6e3320e76a1d516af0ff477f555db0ba1bfc0440
Henry Le <140691753+hen43@users.noreply.github.com>
2023-12-18 16:56:28 -0800

    stuff
    i am stuff

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
+import Froggers from '../assets/robots/froggers.jpg'
+import Kevilry from '../assets/robots/froggers.jpg'
+import Robbie from '../assets/robots/robbie.png'
+
 export default function Gallery() {
     return (
         <section className='gallery'>
             <div className='galleryBox'>
                 <div className='galleryHeader'>
-                    FEATURED ROBOTS
+                    Learn about our robots.
                 </div>
+                <div className='galleryBody'>
+                    <div className='galleryImageContainer'>
+                        <div className='galleryRight'></div>
+                        <img className='galleryImage' src={Froggers} alt={"Froggers"}></img>
+                        <div className='galleryLeft'></div>
+                    </div>
+                </div>
+                <div className='galleryCaption'>Froggers is the first FRC robot we produced in our school. Our robot has the ability to detect and autonomously move towards power cells. In addition, with its intake system, we can collect power cells and transport them around.</div>
             </div>
         </section>
 
